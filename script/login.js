a3e04a04f62db3f468dca7a5096b3842fd759707
AtikHasanSarker <atiksagor24@gmail.com>
2026-03-02 22:52:48 +0600

    Fixed web page hoisting

--- script/login.js
+++ script/login.js
@@ -7,7 +7,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
 
     if(contactNumber == "01234567890" && pin == "4321"){
         alert("Login Successfully.")
-        window.location.assign("/home.html")
+        window.location.assign("home.html")
     }else{
         alert("Login Failed.");
     }
